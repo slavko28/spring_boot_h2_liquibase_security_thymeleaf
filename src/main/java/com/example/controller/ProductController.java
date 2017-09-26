cc9fdc80be45cba00c4a8d2be518996abcee6dc2
Yaroslav Soltys <ce.prosto@gmail.com>
2017-09-26 10:54:40 +0300

    added product controller

---
+++ src/main/java/com/example/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.controller;
+
+import com.example.model.Product;
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+
+    private ProductService service;
+
+    @Autowired
+    public void setService(ProductService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(value = "product", method = RequestMethod.POST)
+    public String save(Product product) {
+        service.save(product);
+        return "redirect:/product/" + product.getId();
+    }
+
+    @RequestMapping("product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", service.getById(id));
+        return "productform";
+    }
+
+    @RequestMapping(value = "product/{id}")
+    public String showOne(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", service.getById(id));
+        return "productshow";
+    }
+
+    @RequestMapping(value = "product/all")
+    public String showAll(Model model) {
+        model.addAttribute("products", service.getAll());
+        return "products";
+    }
+
+    @RequestMapping("product/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        service.delete(id);
+        return "redirect:/products";
+    }
+}
